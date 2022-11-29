ffe87a43043ca2c2febf372cd7a319d34b2bf063
Sarah Mitko <sarahmitko@gmail.com>
2022-11-29 18:02:29 +0100

    added basic show page integrated with devise and added firstname, lastname, username, and bio to user table

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path
+    # check if root_path is right
+  end
+
+
+end
