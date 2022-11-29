e80767f4c0bc723b90e5299a7197eb06f4f1fc0a
Sofia CachoRuschner <sofiamcr@gmail.com>
2022-11-29 15:03:02 +0100

    adding controllers

---
+++ app/controllers/weekly_progresss_controller.rb
@@ -0,0 +1,2 @@
+class WeeklyProgresssController < ApplicationController
+end
---
+++ test/controllers/weekly_progresss_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class WeeklyProgresssControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
