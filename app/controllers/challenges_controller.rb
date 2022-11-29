109edbf96929cc1d4a125de6974dd3e569631d80
Michael-Cool <michaelnatterer@gmx.net>
2022-11-29 17:04:36 +0100

    now we can create Challenges

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,2 +1,20 @@
 class ChallengesController < ApplicationController
+  def new
+    @challenge = Challenge.new # Needed to instantiate the form_with
+  end
+
+  def create
+    @challenge = Challenge.new(challenge_params)
+    if @challenge.save
+      redirect_to challenge_path(@challenge)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:challenge).permit(:name, :goal_qty, :start_date, :end_date, :unit, :challenge_qty, :type, :price, :card_num)
+  end
 end
