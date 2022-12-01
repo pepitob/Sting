e939d49341a755dd6a2b2d3afc87eb94c179c29f
root <hippolytebourban@gmail.com>
2022-12-01 21:43:30 +0100

    fixed integer limit

--- db/migrate/20221201171009_add_activity_id_to_workouts.rb
+++
@@ -1,5 +0,0 @@
-class AddActivityIdToWorkouts < ActiveRecord::Migration[7.0]
-  def change
-    add_column :workouts, :activity_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_01_164618) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_01_202453) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -121,7 +121,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_01_164618) do
     t.datetime "updated_at", null: false
     t.float "distance"
     t.integer "duration"
-    t.integer "activity_id"
+    t.bigint "activity_id"
     t.index ["user_id"], name: "index_workouts_on_user_id"
   end
 
