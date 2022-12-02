bebcdc21e97b9d6548569a8408c3542a7e249fde
Sarah Mitko <sarahmitko@gmail.com>
2022-12-02 11:01:06 +0100

    add column to change integer limit in workouts id

--- db/migrate/20221201202453_change_integer_limit_in_workouts_id.rb
+++ db/migrate/20221201202453_change_integer_limit_in_workouts_id.rb
@@ -1,5 +1,5 @@
 class ChangeIntegerLimitInWorkoutsId < ActiveRecord::Migration[7.0]
   def change
-    change_column :workouts, :activity_id, :integer, limit: 8
+    add_column :workouts, :activity_id, :integer, limit: 8
   end
 end
