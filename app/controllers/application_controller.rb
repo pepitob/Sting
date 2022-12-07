7912b2e17ad709a0babbd224453ba3f6df2b4752
Flora Brossard <florabrossard@Floras-MacBook-Air.local>
2022-12-07 18:07:09 +0100

    cheli done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,9 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     challenges_path(current_user)
   end
+
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
