dae0f4e328997a15a04532c80d848425dece47f1
Sofia CachoRuschner <sofiamcr@gmail.com>
2022-12-07 09:55:41 +0100

    adding migration in weekly progress

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2022_12_06_210318) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_12_07_085131) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -123,6 +121,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_06_210318) do
     t.datetime "updated_at", null: false
     t.string "unit"
     t.boolean "updated", default: false
+    t.float "week_goal"
+    t.float "challenge_goal"
     t.index ["challenge_id"], name: "index_weekly_progresses_on_challenge_id"
     t.index ["user_id"], name: "index_weekly_progresses_on_user_id"
   end
