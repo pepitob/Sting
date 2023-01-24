5e5441982b87d868f6ae0da272eecbcb57f26353
Sofia CachoRuschner <sofiamcr@gmail.com>
2023-01-24 17:07:50 +0100

    deleting migrations

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,3 +1,5 @@
+require "#{Rails.root}/app/services/stripe_checkout_session_service.rb"
+
 Rails.configuration.stripe = {
   publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
   secret_key:      ENV['STRIPE_SECRET_KEY'],
